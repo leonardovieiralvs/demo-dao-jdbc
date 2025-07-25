ad27ea87de88ff337f1fb283a4c98eafb5dc1f64
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2025-07-24 22:13:12 -0300

    seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
 import entities.Department;
+import entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
@@ -8,5 +11,8 @@ public class Program {
 
 		Department dp = new Department(1, "Books");
 		System.out.println(dp);
+
+		Seller seller = new Seller(21, "Leonardo", "leonardo@hotmail.com", new Date(), 1000.0, dp);
+		System.out.println(seller);
 	}
 }
