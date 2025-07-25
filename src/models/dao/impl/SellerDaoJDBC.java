ac255c397d9666bf638c33ae57aa3d1bc4420a2c
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2025-07-24 22:26:34 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/models/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,34 @@
+package models.dao.impl;
+
+import entities.Seller;
+import models.dao.SellerDao;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class SellerDaoJDBC implements SellerDao {
+    @Override
+    public void insert(Seller obj) {
+
+    }
+
+    @Override
+    public void update(Seller obj) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return List.of();
+    }
+}
